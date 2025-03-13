c28fda084e1ef65cf63156aba3a0a8f841093bdf
Rafael Neves <rafaelneves.130@gmail.com>
2025-03-13 15:43:33 -0300

    chore: Connect Redis client for caching

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import router from './routes/routerV1';
 import connectDB from './infrastructure/database/database';
 import env from './config/env';
+import { connectRedis } from './infrastructure/cache/redisClient';
 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use('/api/v1', router);
 
 (async () => {
   await connectDB();
+  await connectRedis();
   app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
   });
