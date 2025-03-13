ddb5b53abeb08c4ec263e56fdd053819a9214697
Rafael Neves <rafaelneves.130@gmail.com>
2025-03-13 17:36:22 -0300

    feat: Add validation middleware for request body

---
+++ src/application/middlewares/validationMiddleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+import { ZodError, ZodSchema } from "zod";
+
+export const validateRequest = (schema: ZodSchema): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    try {
+      req.body = schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(400).json({
+          errors: error.errors.map((e) => ({
+            field: e.path.join('.'),
+            message: e.message,
+          })),
+        });
+      }
+      next(error);
+    }
+  };
+};
